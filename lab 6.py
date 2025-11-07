26981af3a809ca6e8e0ecc95417581eba29311ff
g9b94wysvt-droid <g9b94wysvt@privaterelay.appleid.com>
2025-11-07 18:40:25 +0300

    Create lab 6.py

---
+++ lab 6.py
@@ -0,0 +1,12 @@
+def find_adjacent_negatives (arr): for i in range(len(arr) - 1):
+if arr[il < 0 and arr[i+1] < 0:
+print (f" ({arr[il},
+{arr [i+1]})")
+def
+remove_dup licates (arr):
+seen = set ()
+result = |]
+for item in arr:
+if item not in seen: seen.add (item)
+result.append (item)
+return result
